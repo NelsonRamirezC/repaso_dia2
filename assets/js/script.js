5ebfcb3ca52ac25cd942e566c93eb13c97c3b27d
Nelson Ramirez <xhyskoax@gmail.com>
2023-04-29 11:47:52 -0400

    se agrega consumo api pokémones y mostramos en un card horizontal responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(() => {
     (function main() {
+        //IIFE
         let acumulador = "";
         arrProductos.forEach((producto) => {
             acumulador += `
@@ -9,8 +10,8 @@ $(() => {
                         <div class="card-body">
                             <h5 class="card-title">${producto.nombre}</h5>
                             <p class="card-text">${producto.descripcion}</p>
-                            <a href="./detalle_producto.html?id=${producto.id}" class="btn btn-primary">Ver detalle</a>
-                            <button class="btn-modal btn btn-success" data-id="${producto.id}" data-bs-toggle="modal" data-bs-target="#modalProducto">Modal producto</button>
+                            <a href="./detalle_producto.html?id=${producto.id}" class="my-1 btn btn-primary">Comprar</a>
+                            <button class="btn-modal btn btn-success my-1" data-id="${producto.id}" data-bs-toggle="modal" data-bs-target="#modalProducto">Ver Detalle</button>
                         </div>
                     </div>
                 </div>
@@ -21,9 +22,18 @@ $(() => {
         $("#listadoProductos").html(acumulador);
     })();
 
-    $(".btn-modal").on("click", function (event) {
+    /*  $(".btn-modal").on("click", function (event) {
         let idProducto = event.target.dataset.id;
         cargarDatosModal(idProducto);
+    }); */
+
+    let botonesModal = document.getElementsByClassName("btn-modal");
+    let arrayBotones = [...botonesModal];
+    arrayBotones.forEach((boton) => {
+        boton.addEventListener("click", function (event) {
+            let idProducto = event.target.dataset.id;
+            cargarDatosModal(idProducto);
+        });
     });
 
     /*  window.mostrarModal = function(id) {
@@ -54,7 +64,6 @@ function cargarDatosModal(id) {
 
 function obtenerPokemon(nombrePokemon = 1) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + nombrePokemon;
-
     fetch(url)
         .then((response) => {
             if (response.status == 200) {
@@ -64,9 +73,43 @@ function obtenerPokemon(nombrePokemon = 1) {
             }
         })
         .then((pokemon) => {
-            console.log(pokemon);
+            /* 
+            name,
+            height
+            weight
+            types = [] => type.name
+            sprites.other.official-artwork.front_shiny
+            stats = [] => base_stat: puntaje
+            stats = [] => stat.name: nombre del stat 
+            */
+            let { name, height, weight, types, sprites, stats } = pokemon;
+            let objPokemon = {
+                name,
+                height,
+                weight,
+                types,
+                image: sprites.other["official-artwork"].front_default,
+                stats,
+            };
+            mostrarPokemon(objPokemon);
         })
         .catch((error) => {
             alert(error);
         });
+}
+
+function mostrarPokemon(pokemon) {
+    let contenedorCard = $(".cardPokemon");
+    contenedorCard.find("img").attr("src", pokemon.image);
+    contenedorCard.find(".cardPokemon--title").text(pokemon.name);
+    contenedorCard.find(".cardPokemon--peso").text(pokemon.weight);
+    contenedorCard.find(".cardPokemon--estatura").text(pokemon.height);
+    /* types = [] => type.name */
+
+    let acumulador = "";
+    pokemon.types.forEach((type) => {
+        console.log(type);
+        acumulador += `<li>${type.type.name}</li>`;
+    });
+    contenedorCard.find(".lista--tipos").html(acumulador);
 }
